5e61afa2a5bd3165a896007637d325ca1ab5844d
Leandro Kimura <leandro2ki@gmail.com>
2019-05-01 18:55:10 -0300

    Adding tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '~/pages/Header';
+import Tabs from '~/pages/Tabs';
 
 import { Container } from './styles';
 
@@ -8,6 +9,7 @@ export default function Main() {
   return (
     <Container>
       <Header />
+      <Tabs />
     </Container>
   );
 }
