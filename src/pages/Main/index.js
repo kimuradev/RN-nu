99598148b343b4d5cef6b5f3083dc96f336fe351
Leandro Kimura <leandro2ki@gmail.com>
2019-05-01 19:36:10 -0300

    Adding menu

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '~/pages/Header';
 import Tabs from '~/pages/Tabs';
+import Menu from '~/pages/Menu';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Menu />
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={20} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 197.611,32</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida de Leandro Kimura
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
       <Tabs />
     </Container>
   );
