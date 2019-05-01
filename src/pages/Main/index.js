92982abc42622ec5252429182f3eb59e82b0e2f8
Leandro Kimura <leandro2ki@gmail.com>
2019-05-01 18:43:18 -0300

    Adding header

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '~/pages/Header';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
